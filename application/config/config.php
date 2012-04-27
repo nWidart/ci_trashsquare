e6542cef20e569497ed0b8eda782dab8e08aff14
Nicolas Widart <n.widart@gmail.com>
2012-04-27 22:03:16 +0200

    pagoda config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,9 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ci_testing/';
+
+// $config['base_url']	= 'http://localhost/ci_testing/';
+$config['base_url'] = 'http://ci-trashsquare.pagodabox.com/';
 
 /*
 |--------------------------------------------------------------------------
